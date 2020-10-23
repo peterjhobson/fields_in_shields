796901218aaa8f8ddf33d0435f959fa2a6a94aca
Peter Hobson <peter_hobson@hotmail.co.uk>
2020-10-23 14:35:09 +0100

    Update generatewires.py
    BUG FIX: Added 3D plotting import

--- generatewires.py
+++ generatewires.py
@@ -14,6 +14,7 @@ import time
 
 import numpy as np
 from matplotlib import pyplot as plt
+from mpl_toolkits.mplot3d import Axes3D
 
 from modules import targetpoints, biotsavart, sphericalharmonics, componentfunctions
 
